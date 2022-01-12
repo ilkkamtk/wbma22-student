cbb8ddd15f2890e40bb8bd7b029a2eaedce9b0f2
mopfi <mattipe@gmail.com>
2022-01-12 11:26:28 +0200

    add ApiHooks.js, refactor, task http-c ready

--- components/List.js
+++ components/List.js
@@ -1,38 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {FlatList} from 'react-native';
-import {baseUrl} from '../utils/variables';
+import {useMedia} from '../hooks/ApiHooks';
 import ListItem from './ListItem';
 
 const List = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-  // let mediaArray = [];
-  const loadMedia = async (start = 0, limit = 10) => {
-    try {
-      const response = await fetch(
-        `${baseUrl}media?start=${start}&limit=${limit}`
-      );
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      const json = await response.json();
-      const media = await Promise.all(
-        json.map(async (item) => {
-          const response = await fetch(baseUrl + 'media/' + item.file_id);
-          const mediaData = await response.json();
-          // console.log(mediaData);
-          return mediaData;
-        })
-      );
-      setMediaArray(media);
-      console.log(mediaArray);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  // Call loadMedia() only once when the component is loaded
-  useEffect(() => {
-    loadMedia(0, 5);
-  }, []);
+  const {mediaArray} = useMedia();
 
   return (
     <FlatList
---
+++ hooks/ApiHooks.js
@@ -0,0 +1,37 @@
+import {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const loadMedia = async (start = 0, limit = 10) => {
+    try {
+      const response = await fetch(
+        `${baseUrl}media?start=${start}&limit=${limit}`
+      );
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      const json = await response.json();
+      const media = await Promise.all(
+        json.map(async (item) => {
+          const response = await fetch(baseUrl + 'media/' + item.file_id);
+          const mediaData = await response.json();
+          // console.log(mediaData);
+          return mediaData;
+        })
+      );
+      setMediaArray(media);
+      console.log(mediaArray);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  // Call loadMedia() only once when the component is loaded
+  useEffect(() => {
+    loadMedia(0, 5);
+  }, []);
+
+  return {mediaArray};
+};
+
+export {useMedia};
